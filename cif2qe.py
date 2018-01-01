20aa88e99c1f812797b3e976a28a797d97df5790
Mitsuaki Kwamura <kawamitsuaki@gmail.com>
2018-01-01 21:31:29 +0900

    Use seekpath

--- cif2qe.py
+++ cif2qe.py
@@ -3,24 +3,26 @@ import sys
 import numpy
 import pymatgen
 from sssp import pseudo_dict, ecutwfc_dict, ecutrho_dict, valence_dict
+import seekpath
 
 args = sys.argv
 
 structure = pymatgen.Structure.from_file(args[1])
-
-structure = structure.get_primitive_structure()
-structure = structure.get_primitive_structure()
-structure = structure.get_primitive_structure()
-structure = structure.get_primitive_structure()
-
 structure.remove_oxidation_states()
 
-avec = structure.lattice.matrix
-bvec = structure.lattice.reciprocal_lattice.matrix
-print(bvec)
-nat = len(structure.species)
-typ = set(structure.species)
+num2atom = {str(pymatgen.Element(str(spc)).number):str(spc) for spc in structure.species}
+
+skp = seekpath.get_explicit_k_path((structure.lattice.matrix, structure.frac_coords,
+                         [pymatgen.Element(str(spc)).number for spc in structure.species]))
+
+avec = skp["primitive_lattice"]
+bvec = skp["reciprocal_primitive_lattice"]
+pos = skp["primitive_positions"]
+nat = len(skp["primitive_types"])
+atom = [num2atom[str(skp["primitive_types"][iat])] for iat in range(nat)]
+typ = set(atom)
 ntyp = len(typ)
+
 ecutwfc = 0.0
 ecutrho = 0.0
 for ityp in typ:
@@ -32,10 +34,7 @@ for ii in range(3):
     nq[ii] = round(norm / 0.3359385398275)
     print(norm)
 nelec = 0.0
-for iat in range(nat):
-    nelec += valence_dict[str(structure.species[iat])]
-
-print(structure)
+for iat in range(nat): nelec += valence_dict[atom[iat]]
 #
 # scf.in : Charge density
 #
@@ -69,8 +68,7 @@ with open("scf.in", 'w') as f:
     print("ATOMIC_POSITIONS crystal", file=f)
     for iat in range(nat):
         print(" %s %f %f %f" % (
-            structure.species[iat],
-            structure.frac_coords[iat][0],structure.frac_coords[iat][1],structure.frac_coords[iat][2]), file=f)
+            atom[iat], pos[iat][0],pos[iat][1],pos[iat][2]), file=f)
     #
     print("K_POINTS automatic", file=f)
     print(" %d %d %d 0 0 0" % (nq[0]*2, nq[1]*2, nq[2]*2), file=f)
@@ -108,8 +106,7 @@ with open("nscf.in", 'w') as f:
     print("ATOMIC_POSITIONS crystal", file=f)
     for iat in range(nat):
         print(" %s %f %f %f" % (
-            structure.species[iat],
-            structure.frac_coords[iat][0],structure.frac_coords[iat][1],structure.frac_coords[iat][2]), file=f)
+            atom[iat], pos[iat][0],pos[iat][1],pos[iat][2]), file=f)
     #
     print("K_POINTS automatic", file=f)
     print(" %d %d %d 0 0 0" % (nq[0]*4, nq[1]*4, nq[2]*4), file=f)
@@ -146,8 +143,13 @@ with open("band.in", 'w') as f:
     print("ATOMIC_POSITIONS crystal", file=f)
     for iat in range(nat):
         print(" %s %f %f %f" % (
-            structure.species[iat],
-            structure.frac_coords[iat][0],structure.frac_coords[iat][1],structure.frac_coords[iat][2]), file=f)
+            atom[iat], pos[iat][0],pos[iat][1],pos[iat][2]), file=f)
+    print("K_POINTS crystal", file=f)
+    print(len(skp["explicit_kpoints_rel"]), file=f)
+    for ik in range(len(skp["explicit_kpoints_rel"])):
+        print(" %f %f %f 1.0" % (
+            skp["explicit_kpoints_rel"][ik][0], skp["explicit_kpoints_rel"][ik][1], skp["explicit_kpoints_rel"][ik][2]),
+              file=f)
 #
 # bands.in : Read by bands.x
 #
@@ -201,9 +203,9 @@ with open("elph.in", 'w') as f:
     print("!         last_q = ", file=f)
     print("        fildvscf = \'dv\'", file=f)
     print(" electron_phonon = \'lambda_tetra\'", file=f)
-    print("             nk1 = %d" % nq[0]*4, file=f)
-    print("             nk2 = %d" % nq[1]*4, file=f)
-    print("             nk3 = %d" % nq[2]*4, file=f)
+    print("             nk1 = %d" % (nq[0]*4), file=f)
+    print("             nk2 = %d" % (nq[1]*4), file=f)
+    print("             nk3 = %d" % (nq[2]*4), file=f)
     print("/", file=f)
     print("&INPUTA2F", file=f)
     print(" nfreq = %d" % 100, file=f)
@@ -262,9 +264,9 @@ with open("phdos.in", 'w') as f:
     print("    dos = .true.", file=f)
     print("    asr = \'crystal\'", file=f)
     print("  flfrc = \'ifc.dat\'", file=f)
-    print("    nk1 = %d" % nq[0]*2, file=f)
-    print("    nk2 = %d" % nq[1]*2, file=f)
-    print("    nk3 = %d" % nq[2]*2, file=f)
+    print("    nk1 = %d" % (nq[0]*2), file=f)
+    print("    nk2 = %d" % (nq[1]*2), file=f)
+    print("    nk3 = %d" % (nq[2]*2), file=f)
     print("   ndos = %d" % 100, file=f)
     print("/", file=f)
 #
@@ -304,6 +306,15 @@ with open(args[2] + ".win", 'w') as f:
     print("", file=f)
     print("wannier_plot_supercell = 3", file=f)
     print("begin kpoint_path", file=f)
+    for ipath in range(len(skp["path"])):
+        start = skp["explicit_segments"][ipath][0]
+        final = skp["explicit_segments"][ipath][1] - 1
+        print("%s %f %f %f %s %f %f %f" % (
+            skp["explicit_kpoints_labels"][start],
+            skp["explicit_kpoints_rel"][start][0], skp["explicit_kpoints_rel"][start][1],  skp["explicit_kpoints_rel"][start][2],
+            skp["explicit_kpoints_labels"][final],
+            skp["explicit_kpoints_rel"][final][0], skp["explicit_kpoints_rel"][final][1],  skp["explicit_kpoints_rel"][final][2]),
+              file=f)
     print("end kpoint_path", file=f)
     print("", file=f)
     print("mp_grid = 4 4 4", file=f)
@@ -317,8 +328,7 @@ with open(args[2] + ".win", 'w') as f:
     print("begin atoms_frac", file=f)
     for iat in range(nat):
         print(" %s %f %f %f" % (
-            structure.species[iat],
-            structure.frac_coords[iat][0],structure.frac_coords[iat][1],structure.frac_coords[iat][2]), file=f)
+            atom[iat], pos[iat][0],pos[iat][1],pos[iat][2]), file=f)
     print("end atoms_frac", file=f)
     print("", file=f)
     print("begin kpoints", file=f)
