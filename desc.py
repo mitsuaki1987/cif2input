2c5af32356aba95ee26d475d60b5cdc5e41ec89b
mitsuaki1987 <kawamitsuaki@gmail.com>
2019-10-09 16:24:57 +0900

    Add COMMBO tutorial

---
+++ desc.py
@@ -0,0 +1,155 @@
+#!/usr/bin/python3
+import sys
+import seekpath
+import pymatgen
+from pymatgen.analysis.structure_matcher import StructureMatcher
+import numpy
+from pymatgen.core.periodic_table import get_el_sp
+
+
+if __name__ == '__main__':
+
+    valence_dict = {
+        "H": 0,
+        "He": 0,
+        "Li": 1,
+        "Be": 1.5,
+        "B": 3,
+        "C": 0,
+        "N": 1,
+        "O": -0.125,
+        "F": -0.5,
+        "Ne": 0,
+        "Na": 1,
+        "Mg": 2,
+        "Al": 3,
+        "Si": 0.5,
+        "P": 0.833333,
+        "S": 1.42857,
+        "Cl": 3.125,
+        "Ar": 0,
+        "K": 1,
+        "Ca": 2,
+        "Sc": 3,
+        "Ti": 3,
+        "V": 2.8,
+        "Cr": 2.75,
+        "Mn": 3,
+        "Fe": 2.75,
+        "Co": 1,
+        "Ni": 1.66667,
+        "Cu": 1.5,
+        "Zn": 2,
+        "Ga": 3,
+        "Ge": 3,
+        "As": 1.66667,
+        "Se": 2.66667,
+        "Br": 2.5,
+        "Kr": 2,
+        "Rb": 1,
+        "Sr": 2,
+        "Y": 3,
+        "Zr": 2,
+        "Nb": 3,
+        "Mo": 3.33333,
+        "Tc": 3,
+        "Ru": 3,
+        "Rh": 2.5,
+        "Pd": 3,
+        "Ag": 1.5,
+        "Cd": 2,
+        "In": 2,
+        "Sn": 3,
+        "Sb": 1.66667,
+        "Te": 2.5,
+        "I": 2.5,
+        "Xe": 4,
+        "Cs": 1,
+        "Ba": 2,
+        "La": 3,
+        "Ce": 3.5,
+        "Pr": 3.5,
+        "Nd": 3,
+        "Pm": 3,
+        "Sm": 2.5,
+        "Eu": 2.5,
+        "Gd": 3,
+        "Tb": 3.5,
+        "Dy": 3,
+        "Ho": 3,
+        "Er": 3,
+        "Tm": 2.5,
+        "Yb": 2.5,
+        "Lu": 3,
+        "Hf": 4,
+        "Ta": 5,
+        "W": 3.33333,
+        "Re": 3.71429,
+        "Os": 3,
+        "Ir": 2.14286,
+        "Pt": 2,
+        "Au": 2,
+        "Hg": 1.5,
+        "Tl": 2,
+        "Pb": 2,
+        "Bi": 4,
+        "Po": 2.5,
+        "At": 3,
+        "Rn": 5,
+        "Fr": 1.5,
+        "Ra": 2,
+        "Ac": 3,
+        "Th": 4,
+        "Pa": 4.5,
+        "U": 4.5,
+        "Np": 5,
+        "Pu": 4.5,
+        "Am": 4.5,
+        "Cm": 3.5,
+        "Bk": 3.5,
+        "Cf": 3,
+        "Es": 3,
+        "Fm": 3,
+    }
+
+    matcher = StructureMatcher()
+    args = sys.argv
+    #
+    # Read All files specified as command-line arguments
+    #
+    for ifile in range(len(args)-1):
+        cif_file = args[ifile+1]
+        #
+        # PyMatGen structure from CIF file
+        #
+        structure = pymatgen.Structure.from_file(cif_file)
+        structure.remove_oxidation_states()
+        #
+        # Refine 3-folded Wyckoff position
+        #
+        frac_coord2 = numpy.array(structure.frac_coords)
+        for ipos in range(len(frac_coord2)):
+            for iaxis in range(3):
+                coord3 = frac_coord2[ipos, iaxis] * 6.0
+                if abs(round(coord3) - coord3) < 0.001:
+                    frac_coord2[ipos, iaxis] = float(round(coord3)) / 6.0
+        #
+        # Disordered structure raises AttributeError. So it is skipped.
+        #
+        try:
+            skp = seekpath.get_explicit_k_path((structure.lattice.matrix, frac_coord2,
+                                               [pymatgen.Element(str(spc)).number for spc in structure.species]))
+            structure2 = pymatgen.Structure(skp["primitive_lattice"],
+                                            skp["primitive_types"], skp["primitive_positions"])
+        except AttributeError:
+            print("Fractional occupancy, may be disordered.")
+            continue
+        #
+        vol = structure2.volume
+        nat = len(structure2.atomic_numbers)
+        atom = [str(get_el_sp(iat)) for iat in skp["primitive_types"]]
+        oxidization = 0
+        for iat in atom:
+            oxidization += valence_dict[iat]
+        print(cif_file, vol/nat, oxidization/nat)
+        # print(structure2.distance_matrix)
---
+++ inove.py
@@ -0,0 +1,151 @@
+#!/usr/bin/python3
+import os
+import pymatgen
+import seekpath
+from pymatgen.core.periodic_table import get_el_sp
+from sssp import pseudo_dict, ecutwfc_dict, ecutrho_dict
+from xml.etree import ElementTree
+import combo
+import subprocess
+import numpy
+
+
+def load_data():
+    temp = numpy.asarray(numpy.loadtxt('desc.csv', skiprows=1, delimiter=','))
+    descriptor0 = temp[:, 0:3]
+    filename0 = temp[:, 3]
+    return descriptor0, filename0
+
+
+class Simulator:
+    def __init__(self):
+        _, self.filename = load_data()
+
+    def __call__(self, action):
+
+        structure = pymatgen.Structure.from_file(self.filename[action])
+        structure.remove_oxidation_states()
+        frac_coord2 = numpy.array(structure.frac_coords)
+        for ipos in range(len(frac_coord2)):
+            for iaxis in range(3):
+                coord3 = frac_coord2[ipos, iaxis] * 6.0
+                if abs(round(coord3) - coord3) < 0.001:
+                    frac_coord2[ipos, iaxis] = float(round(coord3)) / 6.0
+        #
+        skp = seekpath.get_path((structure.lattice.matrix, frac_coord2,
+                                 [pymatgen.Element(str(spc)).number for spc in structure.species]))
+        #
+        # Lattice information
+        #
+        avec = skp["primitive_lattice"]
+        bvec = skp["reciprocal_primitive_lattice"]
+        pos = skp["primitive_positions"]
+        nat = len(skp["primitive_types"])
+        atom = [str(get_el_sp(iat)) for iat in skp["primitive_types"]]
+        typ = set(atom)
+        ntyp = len(typ)
+        #
+        # WFC and Rho cutoff
+        #
+        ecutwfc = 0.0
+        ecutrho = 0.0
+        for ityp in typ:
+            if ecutwfc < ecutwfc_dict[str(ityp)]:
+                ecutwfc = ecutwfc_dict[str(ityp)]
+            if ecutrho < ecutrho_dict[str(ityp)]:
+                ecutrho = ecutrho_dict[str(ityp)]
+        #
+        # k grid
+        #
+        nk = numpy.zeros(3, numpy.int_)
+        for ii in range(3):
+            norm = numpy.sqrt(numpy.dot(bvec[ii][:], bvec[ii][:]))
+            nk[ii] = round(norm / 0.16)
+            if nk[ii] == 0:
+                nk[ii] = 1
+        #
+        # SCF file
+        #
+        with open("scf.in", 'w') as f:
+            print("&CONTROL", file=f)
+            print(" calculation = \'scf\'", file=f)
+            print("  pseudo_dir = '/home/kawamura/work/pseudo/'", file=f)
+            # print("  pseudo_dir = '/work/i0012/i001200/pseudo/'", file=f)
+            print("/", file=f)
+            print("&SYSTEM", file=f)
+            print("       ibrav = 0", file=f)
+            print("         nat = %d" % nat, file=f)
+            print("        ntyp = %d" % ntyp, file=f)
+            print("     ecutwfc = %f" % ecutwfc, file=f)
+            print("     ecutrho = %f" % ecutrho, file=f)
+            print(" occupations = \'tetrahedra_opt\'", file=f)
+            print("/", file=f)
+            print("&ELECTRONS", file=f)
+            print(" mixing_beta = 0.3", file=f)
+            print("/", file=f)
+            print("CELL_PARAMETERS angstrom", file=f)
+            for ii in range(3):
+                print(" %f %f %f" % (avec[ii, 0], avec[ii, 1], avec[ii, 2]), file=f)
+            print("ATOMIC_SPECIES", file=f)
+            for ityp in typ:
+                print(" %s %f %s" % (ityp, pymatgen.Element(ityp).atomic_mass, pseudo_dict[str(ityp)]), file=f)
+            print("ATOMIC_POSITIONS crystal", file=f)
+            for iat in range(nat):
+                print(" %s %f %f %f" % (
+                    atom[iat], pos[iat][0], pos[iat][1], pos[iat][2]), file=f)
+            print("K_POINTS automatic", file=f)
+            print(" %d %d %d 0 0 0" % (nk[0], nk[1], nk[2]), file=f)
+        #
+        # Run DFT
+        #
+        subprocess.call("mpirun -hostfile $PBS_NODEFILE ~/bin/pw.x -nk 28 -in scf.in > scf.out", shell=True)
+        # subprocess.call("mpirun -np 2 ~/bin/pw.x -nk 2 -in scf.in > scf.out", shell=True)
+        #
+        # Extract DOS
+        #
+        xmlfile = os.path.join("pwscf.save/", 'data-file-schema.xml')
+        tree = ElementTree.parse(xmlfile)
+        root = tree.getroot()
+        child = root.find('output').find('band_structure')
+        efermi = float(child.find('fermi_energy').text) * 13.60569228 * 2.0
+        print(efermi)
+        #
+        # DOS file
+        #
+        with open("dos.in", 'w') as f:
+            print("&DOS", file=f)
+            print("      emin = %f" % efermi, file=f)
+            print("      emax = %f" % efermi, file=f)
+            print("    deltae = 0.1", file=f)
+            print("/", file=f)
+        #
+        # Run DOS
+        #
+        subprocess.call("mpirun -hostfile $PBS_NODEFILE ~/bin/dos.x -in dos.in > dos.out", shell=True)
+        # subprocess.call("mpirun -np 2 ~/bin/dos.x -in dos.in > dos.out", shell=True)
+        #
+        with open("pwscf.dos", "r") as f:
+            f.readline()
+            line = f.readline()
+            dos = float(line.split()[1]) / nat
+        #
+        return dos
+
+
+descriptor, filename = load_data()
+descriptor = combo.misc.centering(descriptor)
+policy = combo.search.discrete.policy(test_X=descriptor)
+policy.set_seed(1)
+random_search = policy.random_search(max_num_probes=5, simulator=Simulator())
+bayes_search = policy.bayes_search(max_num_probes=30, simulator=Simulator(), score='TS',
+                                   interval=1, num_rand_basis=5000)
+
+print('f(x)=')
+print(bayes_search.fx[0:bayes_search.total_num_search])
+best_fx, best_action = bayes_search.export_all_sequence_best_fx()
+print('current best')
+print(best_fx)
+print('current best action=')
+print(best_action)
+print('history of chosen actions=')
+print(bayes_search.chosed_actions[0:bayes_search.total_num_search])
